c4f0a4a4672d8de2042542eb8007dc122157d910
mwhall <hallm2533@gmail.com>
2019-06-26 14:18:38 -0300

    Add an example script for how to get Django 2.2 django-wiki compatibility

---
+++ scripts/installdjangowiki.sh
@@ -0,0 +1,6 @@
+#Perform these commands where you would like django-wiki placed
+git clone https://github.com/django-wiki/django-wiki.git
+cd django-wiki
+git fetch origin pull/951/head:django22
+git checkout django22
+pip install .
