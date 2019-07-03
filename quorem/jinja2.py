e2f8193aa59e113c23b61febd6c06695d3a82348
alexmanuele <alexmanuele@gmail.com>
2019-07-03 16:36:35 -0300

    Used jinja filters to fix pagination losing context on page change

--- quorem/jinja2.py
+++ quorem/jinja2.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 from jinja2 import Environment
 from jinja2.runtime import Context
-from .jinja.filters import highlight, add_type, format_pages
+from .jinja.filters import highlight, add_type, format_pages, page_url
 
 def environment(**options):
     env = Environment(**options)
@@ -15,5 +15,6 @@ def environment(**options):
         'highlight' : highlight,
         'add_type': add_type,
         'format_pages':format_pages,
+        'page_url': page_url,
     })
     return env
