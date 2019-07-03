5e4fe50e0326425d1bc4e4a7758e1205445eae85
alexmanuele <alexmanuele@gmail.com>
2019-07-03 14:31:28 -0300

    Fixed pagination on search page.

--- quorem/jinja/filters.py
+++ quorem/jinja/filters.py
@@ -20,3 +20,21 @@ def add_type(context, type_name):
     q_dict = context.copy()
     q_dict['selected']['type'] = type_name
     return render(request, 'search_results.htm', q_dict)
+
+def format_pages(paginator, current_page, neighbors=5):
+    if paginator.num_pages > 2*neighbors:
+        start_index = max(1, current_page-neighbors)
+        end_index = min(paginator.num_pages, current_page + neighbors)
+        if end_index < start_index + 2*neighbors:
+            end_index = start_index + 2*neighbors
+        elif start_index > end_index - 2*neighbors:
+            start_index = end_index - 2*neighbors
+        if start_index < 1:
+            end_index -= start_index
+            start_index = 1
+        elif end_index > paginator.num_pages:
+            start_index -= (end_index-paginator.num_pages)
+            end_index = paginator.num_pages
+        page_list = [f for f in range(start_index, end_index+1)]
+        return page_list[:(2*neighbors + 1)]
+    return paginator.page_range
