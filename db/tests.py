7832a10005351edc1840d149316573fb7f6dd304
alexmanuele <alexmanuele@gmail.com>
2019-08-09 14:43:12 -0300

    Integration tests for search facets

--- db/tests.py
+++ db/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase, LiveServerTestCase
+from django.test import TestCase, LiveServerTestCase, TransactionTestCase
 
 from . import models, views, forms
+from accounts.models import User
 from quorem.wiki import initialize_wiki
 
 from selenium import webdriver
@@ -12,31 +13,35 @@ def chromedriver_init():
     option.add_argument("--headless")
     """
     LOCAL TESTING:
-
+    """
     driver_location = os.getcwd() + '/db/chromedriver'
     driver = webdriver.Chrome(executable_path=driver_location)
 
     """
     #CircleCI config:
     driver = webdriver.Chrome(chrome_options=option)
-    
+    """
     return driver
 
-#Note that functions with prefix "test_" will be run by manage.py test
-class FunctionalTests(LiveServerTestCase):
+#Class seleniumTests instantiates web driver
+class SeleniumTest(LiveServerTestCase):
+    serialized_rollback=True
     def setUp(self):
         self.driver = chromedriver_init()
-        super(FunctionalTests, self).setUp()
+        self.driver.implicitly_wait(10)
+        super(SeleniumTest, self).setUp()
 
     def tearDown(self):
         self.driver.quit()
-        super(FunctionalTests, self).tearDown()
+        super(SeleniumTest, self).tearDown()
 
-    def test_register(self):
+class SearchTest(SeleniumTest):
+    fixtures = ['testdump.json']
+    def test_searchpage(self):
+        #init
         driver = self.driver
-        #nav to web page
         driver.get(self.live_server_url)
-        #navigate to sign up page
+        #click signin page
         signup = driver.find_element_by_xpath('/html/body/header/div[1]/ul/li[1]/a')
         signup.click()
         #find form elements
@@ -50,16 +55,40 @@ class FunctionalTests(LiveServerTestCase):
         password2.send_keys('password123')
         #submit the form
         submit.click()
-        #Try to enter the page.
-        enter = driver.find_element_by_xpath('/html/body/header/div[2]/p/a')
-        enter.click()
-        assert "Investigation List" in driver.page_source
+        #go to seach page
+        driver.get(self.live_server_url + '/search/')
+        #check that facets exist
+        types =  driver.find_elements_by_class_name('typecounts')
+        assert types != None
+        types[0].click()
+        meta = driver.find_elements_by_class_name('metadata')
+        assert meta != None
+        meta[1].click()
+        move_slider = webdriver.ActionChains(driver)
+        slider = driver.find_element_by_xpath('//*[@id="slider-range"]/span[1]')
+        assert slider != None
+        move_slider.click_and_hold(slider).move_by_offset(10,0).release().perform()
+        #search
+        search = driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/button')
+        search.click()
+        #check that search facets have happened
+        category_filter = driver.find_element_by_class_name('btn-outline-primary')
+        meta_filter = driver.find_element_by_class_name('btn-outline-success')
+        range_filter = driver.find_element_by_class_name('btn-outline-warning')
+
+        assert category_filter != None
+        assert meta_filter != None
+        assert range_filter != None
 
 #simply test if the basic forms work
 class BasicFormTests(TestCase):
     #initialize objects into testdb to allow form tests.
     def setUp(self):
         initialize_wiki()
+        #create a test user
+        user = User.objects.create(email="test@hotmail.com", username="testyboi")
+
+
         test_inv = models.Investigation.objects.create(name="Test Inv.", institution='Test co.',
                                         description="A test.")
         test_sample = models.Sample.objects.create(name="T001", investigation_id=test_inv.pk)
@@ -100,3 +129,29 @@ class BasicFormTests(TestCase):
                      'biological_replicate_protocol': test_protocol.pk}
         form = forms.ReplicateForm(data=form_data)
         self.assertTrue(form.is_valid())
+
+#Note that functions with prefix "test_" will be run by manage.py test
+class RegistrationTest(SeleniumTest):
+#    fixtures = ['testdump.json']
+    def test_register(self):
+        driver = self.driver
+        #nav to web page
+        driver.get(self.live_server_url)
+        #navigate to sign up page
+        signup = driver.find_element_by_xpath('/html/body/header/div[1]/ul/li[1]/a')
+        signup.click()
+        #find form elements
+        email = driver.find_element_by_id('id_email')
+        password1 = driver.find_element_by_id('id_password')
+        password2 = driver.find_element_by_id('id_confirm_password')
+        submit = driver.find_element_by_xpath('/html/body/header/div[2]/form/div/button')
+        #enter valid registration information and submit
+        email.send_keys('testemail@gmail.com')
+        password1.send_keys('password123')
+        password2.send_keys('password123')
+        #submit the form
+        submit.click()
+        #Try to enter the page.
+        enter = driver.find_element_by_xpath('/html/body/header/div[2]/p/a')
+        enter.click()
+        assert "Investigation List" in driver.page_source
