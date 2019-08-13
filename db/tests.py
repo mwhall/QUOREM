5e8311165d54162264d093933103cf59778b70e6
alexmanuele <alexmanuele@gmail.com>
2019-08-13 13:53:56 -0300

    Fix webdriver JS issue

--- db/tests.py
+++ db/tests.py
@@ -14,6 +14,7 @@ import os
 def chromedriver_init():
     option = webdriver.ChromeOptions()
     option.add_argument("--headless")
+    option.add_argument("javascript.enabled", True)
     """
     LOCAL TESTING:
 
