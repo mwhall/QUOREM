250ce4075f43dae94adc06ee27a64d7ca046cd61
Mike Hall <hallm2533@gmail.com>
2021-11-03 21:22:33 -0300

    Update launch script to be Docker-friendly

--- scripts/launchquorem.sh
+++ scripts/launchquorem.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python manage.py makemigrations && python manage.py migrate && python manage.py runserver
+conda run -n quorem python manage.py makemigrations
+conda run -n quorem python manage.py migrate
+conda run -n quorem --no-capture-output python manage.py runserver 0.0.0.0:8000 &
+conda run -n quorem --no-capture-output celery -A quorem worker --concurrency 1
