7e7be48b6c754aba2c04cbfdc3804383b622f555
Michael Hall <hallm2533@gmail.com>
2022-09-06 13:28:03 -0300

    Fix order of Django entrypoint so that it doesn't crash on superuser creation on first startup

--- docker/django_entrypoint.sh
+++ docker/django_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#conda run -n quorem python manage.py createsuperuser --noinput --email ${DJANGO_SUPERUSER_EMAIL}
-#conda run -n quorem python manage.py makemigrations
-#conda run -n quorem python manage.py migrate
+conda run -n quorem python manage.py makemigrations
+conda run -n quorem python manage.py migrate
+conda run -n quorem python manage.py createsuperuser --noinput --email ${DJANGO_SUPERUSER_EMAIL}
 conda run -n quorem --no-capture-output python manage.py runserver 0.0.0.0:8000 &
 #conda run -n quorem gunicorn quorem.wsgi --bind 0.0.0.0:8000 &
 conda run -n quorem --no-capture-output celery -A quorem worker --concurrency 1
