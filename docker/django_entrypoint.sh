483fa828213a125497eaa35d623280ae8853a613
Michael Hall <hallm2533@gmail.com>
2022-09-08 03:26:51 -0300

    Make sure collectstatic runs in Docker

--- docker/django_entrypoint.sh
+++ docker/django_entrypoint.sh
@@ -3,7 +3,7 @@ conda run -n quorem python manage.py makemigrations
 conda run -n quorem python manage.py migrate
 conda run -n quorem python manage.py initialize
 conda run -n quorem python manage.py createsuperuser --noinput --email ${DJANGO_SUPERUSER_EMAIL}
-conda run -n quorem python manage.py collectstatic
+conda run -n quorem python manage.py collectstatic --noinput
 conda run -n quorem --no-capture-output python manage.py runserver 0.0.0.0:8000 &
 #conda run -n quorem gunicorn quorem.wsgi --bind 0.0.0.0:8000 &
 conda run -n quorem --no-capture-output celery -A quorem worker --concurrency 1
